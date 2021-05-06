c6fcc3aa5ccaabd66aca909281a1f51b306c85a7
shakilhossainn <proshakil0@gmail.com>
2021-05-06 12:37:19 +0600

    setup tailwind css and complate nav section

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens:{
+      sm:'576px',
+      md:'768px',
+      lg:'992px',
+      xl:'1200px',
+    },
+    extend: {
+      fontFamily:{
+        'poppins':['Poppins', 'sans-serif'],
+        'roboto':['Roboto', 'sans-serif'],
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
